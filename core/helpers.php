b2149847fe9d6d2eb5f81e13496d1e5ede574fd9
Miroslav Krysl <wilyxjam@gmail.com>
2017-10-27 23:50:01 +0200

    helper array_to_dot function

--- core/helpers.php
+++ core/helpers.php
@@ -1,1 +1,25 @@
 <?php
+
+/**
+ * Transform multidimensional array into one-dimensional.
+ * The nested keys in old array is represented in dot notation in new array.
+ * @param array $array
+ * @return array
+ */
+function array_to_dot(array $array): array {
+    $result = [];
+
+    foreach ($array as $key => $value) {
+        if (!is_array($value)) {
+            $result[$key] = $value;
+        }
+        else {
+            $subArray = array_to_dot($value);
+            foreach ($subArray as $subKey => $subValue){
+                $result[$key.'.'.$subKey] = $subValue;
+            }
+        }
+    }
+
+    return $result;
+}
